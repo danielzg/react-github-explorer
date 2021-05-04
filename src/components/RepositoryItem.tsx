aedcd5532e08e9e5172efe2faceec483269bf869
daniel <danielzagocapanema@gmail.com>
2021-05-04 19:11:42 -0300

    first commit

---
+++ src/components/RepositoryItem.tsx
@@ -0,0 +1,14 @@
+import {RepositoryItemProps} from '../interfaces/Repository';
+
+export function RepositoryItem(props: RepositoryItemProps){
+    return (
+        <li>
+            <strong>{props.repository?.name}</strong>
+            <p>{props.repository.description}</p>
+            <a href={props.repository.html_url}>
+                Acessar Repositorio
+
+            </a>
+        </li>
+    )
+}
